a2e3b2adb061e6fed5be33a8af01314b210665d0
Yohana Tesfazgi <yytesfazgi@gmail.com>
2025-05-03 15:37:51 -0400

    Removes old greet command.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,11 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 mod ffmpeg;
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             ffmpeg::start_ffmpeg,
             ffmpeg::stop_ffmpeg,
             ffmpeg::ffmpeg_status
